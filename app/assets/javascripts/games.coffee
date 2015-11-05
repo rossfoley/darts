f23c7d3d84afccfca893d230258c8399d5e7bbb5
Ross Foley <ross3141@gmail.com>
2015-11-05 11:25:11 -0800

    Add game charts

--- app/assets/javascripts/games.coffee
+++ app/assets/javascripts/games.coffee
@@ -1,3 +1,26 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$ ->
+  $('.pie-chart').each ->
+    $(@).highcharts
+      chart:
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      title:
+        text: 'Distribution of Multipliers'
+      tooltip:
+        pointFormat: '{series.name}: <b>{point.y:.0f} ({point.percentage:.1f}%)</b>'
+      plotOptions:
+        pie:
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels:
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.y:.0f} ({point.percentage:.1f}%)</b>'
+            style:
+              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+      series: [
+        name: 'Multipliers',
+        colorByPoint: true,
+        data: $(@).data('chart-series')
+      ]
--- app/services/game_statistics_service.rb
+++ app/services/game_statistics_service.rb
@@ -6,16 +6,36 @@ class GameStatisticsService
   def call
     stats = {}
     multipliers = @game.scores.group_by &:multiplier
-    stats[:singles] = multipliers[1].try(:count) || 0
-    stats[:doubles] = multipliers[2].try(:count) || 0
-    stats[:triples] = multipliers[3].try(:count) || 0
+    stats[:overall] = [
+        {
+            name: 'Singles',
+            y: multipliers[1].try(:count) || 0
+        },
+        {
+            name: 'Doubles',
+            y: multipliers[2].try(:count) || 0
+        },
+        {
+            name: 'Triples',
+            y: multipliers[3].try(:count) || 0
+        }
+    ]
     stats[:teams] = @game.teams.map do |team|
       multipliers = team.scores.where(game: @game).group_by &:multiplier
-      {
-          singles: multipliers[1].try(:count) || 0,
-          doubles: multipliers[2].try(:count) || 0,
-          triples: multipliers[3].try(:count) || 0
-      }
+      [
+          {
+              name: 'Singles',
+              y: multipliers[1].try(:count) || 0
+          },
+          {
+              name: 'Doubles',
+              y: multipliers[2].try(:count) || 0
+          },
+          {
+              name: 'Triples',
+              y: multipliers[3].try(:count) || 0
+          }
+      ]
     end
     stats
   end
