74f3b4fc390abd781b5e7d2729ab724479e690c9
Ross Foley <ross3141@gmail.com>
2015-11-18 13:08:39 -0800

    Destroy rounds on game destroy

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   has_and_belongs_to_many :teams
   has_many :scores
-  has_many :rounds
+  has_many :rounds, dependent: destroy
   serialize :final_scores, Array
 
   validates :teams, length: {is: 2}
