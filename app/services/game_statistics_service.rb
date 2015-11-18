fc79768a829e47e1a726c9a42a54d73819772970
Ross Foley <ross3141@gmail.com>
2015-11-18 14:57:25 -0800

    Use better chart

--- app/services/game_statistics_service.rb
+++ app/services/game_statistics_service.rb
@@ -12,5 +12,12 @@ class GameStatisticsService
           multiplier_series(team.scores.where(game: @game).group_by(&:multiplier))
         end
     }
+    @game.teams.map do |team|
+      multipliers = team.scores.where(game: @game).group_by(&:multiplier)
+      {
+          name: team.name,
+          data: [1, 2, 3].map { |mark| multipliers[mark].try(:count) || 0 }
+      }
+    end
   end
 end
