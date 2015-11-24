618f088689bc8d6ae0c1d41347300a4992e890ff
Ross Foley <ross3141@gmail.com>
2015-11-24 15:24:02 -0800

    Add default ordering to games and teams

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  has_and_belongs_to_many :teams
+  has_and_belongs_to_many :teams, order: 'id ASC'
   has_many :rounds, dependent: :destroy
   has_many :scores, through: :rounds
 
